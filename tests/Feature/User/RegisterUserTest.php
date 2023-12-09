d1378327bbdcfa5e2d05e7cfcd2e4631bcc8d582
ShaklinDmitry <shaklin.ru@mail.ru>
2023-12-09 15:10:28 +0300

    Добавил тест для регистрация пользователя

--- tests/Feature/User/RegisterUserTest.php
+++ tests/Feature/User/RegisterUserTest.php
@@ -2,20 +2,26 @@
 
 namespace Tests\Feature\User;
 
-use App\Modules\Auth\Application\Usecases\RegisterUserCommandInterface;
+use App\Modules\User\Application\Usecases\RegisterUserCommandInterface;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class RegisterUserTest extends TestCase
 {
+    use RefreshDatabase;
 
     public function test_register_user()
     {
-        $login = 'login';
-        $password = 'password';
+        $name = 'name';
+        $email = 'lol@mail.ru';
+        $password = Hash::make('password');
 
         $registerUserCommand = app(RegisterUserCommandInterface::class);
-        $userDTO = $registerUserCommand->execute($login, $password);
+        $userDTO = $registerUserCommand->execute($name, $email, $password);
 
-        $this->assertSame($userDTO->login, $login);
+        $this->assertSame($userDTO->name, $name);
+        $this->assertSame($userDTO->email, $email);
+        $this->assertSame($userDTO->password, $password);
     }
 }
