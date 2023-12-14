61c291c0c22df46ea59dbb0e8ba27fc33e70131d
ShaklinDmitry <shaklin.ru@mail.ru>
2023-12-14 11:46:25 +0300

    В UserDTO добавил guid

--- app/Modules/User/Application/DTO/UserDTO.php
+++ app/Modules/User/Application/DTO/UserDTO.php
@@ -6,12 +6,13 @@ class UserDTO
 {
 
     /**
+     * @param string $guid
      * @param string $name
      * @param string $email
      * @param string $password
      */
-    public function __construct(public readonly string $name, public readonly string $email,
-                                public readonly string $password)
+    public function __construct(public readonly string $guid, public readonly string $name,
+                                public readonly string $email, public readonly string $password)
     {
     }
 
